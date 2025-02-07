10c609570219db8330ce5bd0d45514af66cb22ab
EphemeralSapient <nowayofway@gmail.com>
2025-02-07 14:23:56 +0530

    SSL problem resolve attempt #1

--- lib/common/http.dart
+++ lib/common/http.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:io' show HttpOverrides;
 
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:transcribe/common/global.dart' as global;
+import 'package:transcribe/main.dart' show MyHttpOverrides;
 
 // Conditional import: if running on the web, use get_client_web.dart; otherwise use get_client_io.dart.
 import 'get_client_io.dart' if (dart.library.html) 'get_client_web.dart';
@@ -11,6 +14,10 @@ import 'get_client_io.dart' if (dart.library.html) 'get_client_web.dart';
 class MyHttpClient {
   // Uses the platform-specific getClient() function from the imported file.
   static http.Client getClientInstance() {
+    // If non-web device then accept insecure connection
+    if (!kIsWeb) {
+      HttpOverrides.global = MyHttpOverrides();
+    }
     return getClient();
   }
 
